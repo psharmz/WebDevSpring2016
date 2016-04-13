46c86aa476f95ee6f10d8b371e9d76dc28c430d5
Parul Sharma <psharmz@Paruls-MacBook-Pro.local>
2016-04-12 22:44:06 -0400

    progress on assignment 5

---
+++ public/app.js
@@ -0,0 +1,12 @@
+//declare a base angular module called 'FormBuilderApp' with ngRoute as a dependency
+//ngRoute is used to configure navigation
+'use strict';
+
+(function(){
+
+    'use strict';
+    angular.module("FormBuilderApp", ["ngRoute", 'ngAnimate', 'ui.bootstrap', 'ui.sortable']);
+
+})();
+
+
--- public/assignment/server/services/user.service.server.js
+++ public/assignment/server/services/user.service.server.js
@@ -1,16 +1,67 @@
+var passport         = require('passport');
+var LocalStrategy    = require('passport-local').Strategy;
+var mongoose         = require("mongoose");
+
+
 //take in the the Model as an arguement so we can CRUD the data 
 module.exports = function(app, userModel){
 
 // user service endpoints 
-    app.post("/api/assignment/user", createUser);
-    app.get("/api/assignment/user", findAllUser);
-    app.get("/api/assignment/user/:id", findByIdUser);
-    app.put("/api/assignment/user/:id", updateUser);
-    app.delete("/api/assignment/user/:id", deleteUser);
 
-    //additional
-    // app.get("/api/assignment/user?username=username")
-    // app.get("/api/assignment/user?username=alice&password=wonderland")
+    //assignment 5 add new endpoints
+    var auth = authenticated;
+
+    //configure passport local strategy to intercept POST api/assignment/login
+    app.post("/api/assignment/login", passport.authenticate('local'), login);
+
+    app.get("/api/assignment/loggedin", loggedin);
+    app.post("/api/assignment/logout", logout);
+    app.post('/api/assignment/register', register);
+
+    //refactor existing user Web service to support admin use cases
+    //these endpoints allow admin users to create, read, update, and delete users
+    app.post("/api/assignment/admin/user", createUser);
+    app.get("/api/assignment/admin/user", findAllUser);
+    app.get("/api/assignment/admin/user/:userid", findByIdUser);
+    app.delete("/api/assignment/admin/user/:userid", deleteUser);
+    app.put("/api/assignment/admin/user/:userid", updateUser);
+    
+    passport.use(new LocalStrategy(localStrategy));
+    passport.serializeUser(serializeUser);
+    passport.deserializeUser(deserializeUser);
+
+    //implement strategies
+
+    function localStrategy(username, password, done) {
+        userModel
+            .findUserByCredentials({username: username, password: password})
+            .then(
+                function(user) {
+                    if (!user) { return done(null, false); }
+                    return done(null, user);
+                },
+                function(err) {
+                    if (err) { return done(err); }
+                }
+            );
+    }
+
+    function serializeUser(user, done) {
+        done(null, user);
+    }
+
+    function deserializeUser(user, done) {
+        userModel
+            .findUserById(user._id)
+            .then(
+                function(user){
+                    done(null, user);
+                },
+                function(err){
+                    done(err, null);
+                }
+            );
+    }
 
     // request to create user and respond with all users
     function createUser(req, res){
@@ -23,27 +74,19 @@ module.exports = function(app, userModel){
 
     // request to read all users and respond with all users
     function findAllUser(req, res){
-        // parse the URL for the username
-        // parse the URL for the password 
-        var username = req.query.username;
-        var password = req.query.password;
-        // if there was a username and password in the URL
-        if (username && password){
-            //save it as a 'credentials' object
-            var credentials = {
-              username: username,
-              password: password
-            };
-            var users = userModel.findUserByCredentials(credentials);
-            res.json(users);
-        } else if (password == null){
-            if (username){
-                var user = userModel.findUserByUsername(username);
-                res.json(user);
-            } else {
-                var users = userModel.findAllUsers();
-                res.json(users);
-            }
+        if(isAdmin(req.user)) {
+            userModel
+                .findAllUsers()
+                .then(
+                    function (users) {
+                        res.json(users);
+                    },
+                    function () {
+                        res.status(400).send(err);
+                    }
+                );
+        } else {
+            res.status(403);
         }
     }
 
@@ -73,6 +116,92 @@ module.exports = function(app, userModel){
         var users = userModel.deleteUser(id);
         res.json(users);
     }
+
+    function isAdmin(user) {
+        if(user.roles.indexOf("admin") > 0) {
+            return true
+        }
+        return false;
+    }
+
+    //implement new endpoints: login, loggedin, logout, register
+
+    function login(req, res) {
+        var user = req.user;
+        res.json(user);
+    }
+
+    function loggedin(req, res) {
+        res.send(req.isAuthenticated() ? req.user : '0');
+    }
+
+    function logout(req, res) {
+        req.logOut();
+        res.send(200);
+    }
+
+    function register(req, res) {
+        var newUser = req.body;
+        newUser.roles = ['student'];
+
+        userModel
+            .findUserByUsername(newUser.username)
+            .then(
+                function(user){
+                    if(user) {
+                        res.json(null);
+                    } else {
+                        return userModel.createUser(newUser);
+                    }
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+            .then(
+                function(user){
+                    if(user){
+                        req.login(user, function(err) {
+                            if(err) {
+                                res.status(400).send(err);
+                            } else {
+                                res.json(user);
+                            }
+                        });
+                    }
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    //implement a middleware function isAdmin() to intercept all admin Web Services
+    //verifies user is logged in
+    //verifies user in admin role
+    //if either is false, then respond with a 403
+    //if both are true, then invoke callback next()
+
+    function isAdmin (req, res, next) {
+        user = req.user
+        if (user.roles.indexOf("admin") < 1) {
+            res.send(403);
+        } else if (!req.isAuthenticated()) {
+            res.send(403); 
+        }
+        } else {
+            next();
+        }
+    }
+
+    function authenticated (req, res, next) {
+        if (!req.isAuthenticated()) {
+            res.send(401);
+        } else {
+            next();
+        }
+    }
+
 }
 
 
