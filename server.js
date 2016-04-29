7cae802c2720d6df9ac83da28e8cb963473c80c8
Parul Sharma <psharmz@Paruls-MacBook-Pro.local>
2016-04-29 07:47:59 -0400

    trying to get openshift to work 23232 try

--- server.js
+++ server.js
@@ -50,16 +50,13 @@ http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
 });
 
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
-// var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-// var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 // app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-// app.listen(port, ipaddress);
+// app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 
-http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
-    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
-    server();
-});
+app.listen(port, ip);
+
+
 
