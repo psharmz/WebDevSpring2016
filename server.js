fea496eb2eeebc30bc1e95c66e2dca2ce45d0c20
Parul Sharma <psharmz@Paruls-MacBook-Pro.local>
2016-04-29 08:06:01 -0400

    server.js

--- server.js
+++ server.js
@@ -14,11 +14,14 @@ var session = require('express-session');
 // default to a 'localhost' configuration:
 var connection_string = '127.0.0.1:27017/webdevelopment';
 
+ mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/
+
 // var connectionString = 'mongodb://127.0.0.1:27017/test';
 
 // if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    connectionString = 
+    // process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        // process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
@@ -59,9 +62,6 @@ http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
 // app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-
-
 
 
 
