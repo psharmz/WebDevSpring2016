4b81f21141c9f2792ff000a3b841d0e46006c3fc
Parul Sharma <psharmz@Paruls-MacBook-Pro.local>
2016-04-29 07:43:42 -0400

    server.js

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ var session = require('express-session');
 
 var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
-}
+// }
 
 
 // connect to the database
